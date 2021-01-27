89e5ccb5ab1f09a33c028fc7a12bd299810e94fd
Elizabete Fabri <elizabete958@gmail.com>
2021-01-26 22:22:11 -0300

    Jogo da Velha

---
+++ velha.js
@@ -0,0 +1,26 @@
+var jogador = null;
+var jogadorSelecionado = document.getElementById(
+  (elementId: 'jogador-selecionado')
+);
+
+mudarJogador((valor: 'X'));
+
+function escolherQuadrado(id) {
+  var quadrado = document.getElementById(id);
+  if (quadrado.innerHTML !== '-') {
+    return;
+  }
+  quadrado.innerHTML = jogador;
+  quadrado.style.color = '#000';
+
+  if (jogador === 'X') {
+    jogador = 'O';
+  } else {
+    jogador = 'X';
+  }
+  mudarJogador(jogador);
+}
+function mudarJogador(valor) {
+  jogador = valor;
+  jogadorSelecionado.innerHTML = jogador;
+}
